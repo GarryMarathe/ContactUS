49be9d0ae03427f520855b96cad189b72883ffd4
GauravMarathe <gauravmarathe6468@gmail.com>
2024-01-10 22:12:52 +0530

    form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Form = () => {
   return (
     <form>
-     <h1>Form</h1> 
+     <h1>Contact <span>Here</span></h1> 
+     <input type="text" name="name" id="" />
+     <input type="email" name="email" id="" />
+     <input type="phone" name="phone" id="" />
+
+     <textarea name="message" id="" cols="30" rows="10"/>
+
+     <button type="submit">Send</button>
     </form>
   )
 }
