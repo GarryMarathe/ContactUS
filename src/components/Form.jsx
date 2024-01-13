f7e9a332accaa365e02d73e534c67319a0be9fdb
GauravMarathe <gauravmarathe6468@gmail.com>
2024-01-14 00:11:55 +0530

    form post3

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -27,7 +27,7 @@ const Form = () => {
      <input type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder='example@gmail.com'/>
      <input type="phone" name="phone" id="" onChange={handleChange} value={data.phone} placeholder='+91'/>
 
-     <textarea name="message" id="" cols="40" rows="20" onChange={handleChange} value={data.message} placeholder='type here....'/>
+     <textarea name="message" id="" cols="30" rows="10" onChange={handleChange} value={data.message} placeholder='type here....'/>
 
      <button type="submit">Send</button>
     </form>
