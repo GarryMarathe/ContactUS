acd87d82e651013a4b82408aac10e7dd391600e0
GauravMarathe <gauravmarathe6468@gmail.com>
2024-01-14 00:27:18 +0530

    form post4

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,33 +4,43 @@ import { useState } from 'react'
 
 const Form = () => {
 
-    const [data, setData] = useState({name: "", email: "", phone: "", message: ""});
-    const handleChange = (e) => {
+  const [data, setData] = useState({ name: "", email: "", phone: "", message: "" });
+  const handleChange = (e) => {
 
-      const name = e.target.name;
-      const value = e.target.value;
-      setData({...data, [name]: value});
+    const name = e.target.name;
+    const value = e.target.value;
+    setData({ ...data, [name]: value });
 
-    }
+  }
 
-    const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
 
-      e.preventDefault()
-      alert(data)
+    e.preventDefault()
+    alert(JSON.stringify(data));
 
 
-    }
+  }
   return (
-    <form method='post' onSubmit={handleSubmit}>
-     <h1>Contact <span>Here</span></h1> 
-     <input type="text" name="name" id="" onChange={handleChange} value={data.name} placeholder='Enter name'/>
-     <input type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder='example@gmail.com'/>
-     <input type="phone" name="phone" id="" onChange={handleChange} value={data.phone} placeholder='+91'/>
 
-     <textarea name="message" id="" cols="30" rows="10" onChange={handleChange} value={data.message} placeholder='type here....'/>
+    <div className="contact-container">
+      <div className="contact-heading">
+        <h1>Contact Me</h1>
+      </div>
 
-     <button type="submit">Send</button>
-    </form>
+      <div className="contact-form">
+        <form method='post' onSubmit={handleSubmit}>
+          <h1>Contact <span>Here</span></h1>
+          <input type="text" name="name" id="" onChange={handleChange} value={data.name} placeholder='Enter name' />
+          <input type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder='example@gmail.com' />
+          <input type="phone" name="phone" id="" onChange={handleChange} value={data.phone} placeholder='+91' />
+
+          <textarea name="message" id="" cols="30" rows="10" onChange={handleChange} value={data.message} placeholder='type here....' />
+
+          <button type="submit">Send</button>
+        </form>
+
+      </div>
+    </div>
   )
 }
 
