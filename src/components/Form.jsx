4fa1e8c06babcc69e73cba53f5f0615af679561f
GauravMarathe <gauravmarathe6468@gmail.com>
2024-01-13 23:18:04 +0530

    form post1

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -16,11 +16,11 @@ const Form = () => {
   return (
     <form method='post' onSubmit={handleSubmit}>
      <h1>Contact <span>Here</span></h1> 
-     <input type="text" name="name" id="" placeholder='Enter name'/>
-     <input type="email" name="email" id="" placeholder='example@gmail.com'/>
-     <input type="phone" name="phone" id="" placeholder='+91'/>
+     <input type="text" name="name" id="" onChange={handleChange} value={data.name} placeholder='Enter name'/>
+     <input type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder='example@gmail.com'/>
+     <input type="phone" name="phone" id="" onChange={handleChange} value={data.phone} placeholder='+91'/>
 
-     <textarea name="message" id="" cols="40" rows="20" placeholder='type here....'/>
+     <textarea name="message" id="" cols="40" rows="20" onChange={handleChange} value={data.message} placeholder='type here....'/>
 
      <button type="submit">Send</button>
     </form>
