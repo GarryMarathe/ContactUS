7d4260b7fea34c27db9169f3468665a3358da481
GauravMarathe <gauravmarathe6468@gmail.com>
2024-01-14 00:38:36 +0530

    form post5

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,13 +13,19 @@ const Form = () => {
 
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = () => {
 
-    e.preventDefault()
-    alert(JSON.stringify(data));
+    const toEmail = "gauravmarathe6468@gmail.com";
+    const subject = encodeURIComponent("User Inquiry");
+    const body = encodeURIComponent(`Message: ${data.message}`);
+
+    const mailtoLink = `mailto:${toEmail}?subject=${subject}&body=${body}`;
+
+    window.location.href = mailtoLink;
 
 
   }
+  
   return (
 
     <div className="contact-container">
@@ -28,13 +34,13 @@ const Form = () => {
       </div>
 
       <div className="contact-form">
-        <form method='post' onSubmit={handleSubmit}>
+        <form method='post' onSubmit={handleSubmit} dataform>
           <h1>Contact <span>Here</span></h1>
-          <input type="text" name="name" id="" onChange={handleChange} value={data.name} placeholder='Enter name' />
-          <input type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder='example@gmail.com' />
-          <input type="phone" name="phone" id="" onChange={handleChange} value={data.phone} placeholder='+91' />
+          <input type="text" name="name" id="" onChange={handleChange} value={data.name} placeholder='Enter name' required/>
+          <input type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder='example@gmail.com' required />
+          <input type="phone" name="phone" id="" onChange={handleChange} value={data.phone} placeholder='+91' required/>
 
-          <textarea name="message" id="" cols="30" rows="10" onChange={handleChange} value={data.message} placeholder='type here....' />
+          <textarea name="message" id="" cols="30" rows="10" onChange={handleChange} value={data.message} placeholder='type here....' required/>
 
           <button type="submit">Send</button>
         </form>
